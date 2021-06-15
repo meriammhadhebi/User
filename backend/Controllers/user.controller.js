f039e85d1e0a24bc3dd689686993cad5c2acb70b
Mhadhebi Meriam <meriam.mhadhebi@esprit.tn>
2021-06-15 23:36:24 +0100

    Register/Login

--- backend/Controllers/user.controller.js
+++ backend/Controllers/user.controller.js
@@ -1,27 +1,45 @@
-const mongoose = require('mongoose');
-const UserModel = require('../Models/user.model');
+const mongoose = require("mongoose");
+const UserModel = require("../Models/user.model");
+const bcrypt = require("bcrypt");
 
 const createUser = async (req, res, next) => {
-    const {  Nom, Prenom, Email , Tel ,Password, Role} = req.body;
-    const User = {  Nom, Prenom, Email , Tel,Password, Role };
-    const newUser = new UserModel(User);
-    try {
-      await newUser.save();
-      res.status(201).json(newUser);
-    } catch (error) {
-      next(error);
-    }
-  };
-const Login = async (req, res,next) => {
-    try {
-      const user = await UserModel.findOne({Email:req.body.Email})
-      return res.json(user);
-    } catch (error) {
-      next(error);
-    }
-  };
+  const { Nom, Prenom, Email, Tel, Role } = req.body;
+  salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(req.body.Password, salt);
+  const User = { Nom, Prenom, Email, Tel, Password: hashedPassword, Role };
+  const newUser = new UserModel(User);
+  try {
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
+const Login = async (req, res, next) => {
+  const { Email, Password } = req.body;
+  const user = await UserModel.findOne({ Email });
+  if (!user) {
+    return res
+      .status(400)
+      .json({ errors: [{ msg: 'User doesnt exist' }] });
+  }
+  try {
 
-  module.exports = {
-    createUser,
-    Login
-    }
+      const isMatch = await bcrypt.compare(Password, user.Password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Not Allowed' }] });
+      }
+      else{
+        return res.json(user);}
+  } catch {
+    return res.json(500).send();
+  }
+};
+
+module.exports = {
+  createUser,
+  Login,
+};
